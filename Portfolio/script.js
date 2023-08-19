324252be0ca6cd4ad106d6e85d9515aea7702bd9
JanaiKasle <122558971+JanaiKasle@users.noreply.github.com>
2023-08-19 19:14:51 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['<i> Web</i> Designer.', ' &amp; Full Stack Developer.'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 100,
+    loop:true
+  });
